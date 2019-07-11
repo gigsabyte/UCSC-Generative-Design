b65fc712cd7c619ceebd89fd9b4cea43e27f4757
Gigi Bachtel <gbachtel@ucsc.edu>
2019-07-11 13:26:05 -0700

    Basic ASG3 setup
    IDK what the assignment is yet so I just cleared out ASG2 code

---
+++ docs/code/asg3/main.js
@@ -0,0 +1,15 @@
+/*
+ * code/asg4/main.js
+ * holds main(), the function that initializes all p5 canvases
+ * and calls event handler initialization as needed
+ * written by gigsabyte
+ */
+
+var canvases = [];
+
+function main() {
+	let asg3p5 = new p5(asg3, 'asg3');
+	canvases.push(asg3p5);
+
+	initEventHandlers();
+}
